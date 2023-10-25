1125514fbbcf2dd5179262eaba7e8847b7ea8793
VishalTawase <vishalshahuraj.tawase2021@vitstudent.ac.in>
2023-10-25 11:38:22 +0530

    first commit

---
+++ controller/studentRoute.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const studentSchema = require("../model/studentSchema");
+const studentRoute = express.Router();
+const mongoose = require("mongoose");
+
+// studentRoute.post()
+studentRoute.post("/create-student",(req,res)=>{
+    studentSchema.create(req.body, (err,data) => {
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+})
+
+studentRoute.get("/",(req,res)=>{
+    studentSchema.find((err,data)=>{
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+})
+studentRoute.route("/update-student/:id")
+.get((req,res)=>{
+    studentSchema.findById(mongoose.Types.ObjectId(req.params.id),(err,data)=>{
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+}).put((req,res)=>{
+    studentSchema.findByIdAndUpdate(mongoose.Types.ObjectId(req.params.id),
+    {$set: req.body},
+    (err,data)=>{
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+})
+
+
+//studentRoute.get
+//studentRoute.delete()
+//_id -> 652cc55e5ffd72e7a415d6f7
+//http://localhost:4000/studentRoute/update-student/652cc55e5ffd72e7a415d6f7
+
+studentRoute.delete("/delete-student/:id",(req,res)=>{
+    studentSchema.findByIdAndRemove(mongoose.Types.ObjectId(req.params.id),
+    (err,data)=>{
+        if(err)
+            return err;
+        else
+            res.json(data);
+    })
+})
+
+
+
+module.exports = studentRoute;
